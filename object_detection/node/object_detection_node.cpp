c57e72d12d998244ef1c556a5e7f8bcb2ffb8bdd
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-07-08 23:03:01 -0400

    WIP on object detection node

--- object_detection/node/object_detection_node.cpp
+++ object_detection/node/object_detection_node.cpp
@@ -1,9 +1,25 @@
 #include <cv_bridge/cv_bridge.h>
 #include <image_transport/image_transport.h>
+#include <ros/package.h>
 #include <ros/ros.h>
 #include <sensor_msgs/image_encodings.h>
 
+#include <opencv2/core/utils/filesystem.hpp>
+
 #include "object_detection/object_detector.hpp"
+
+namespace object_detection {
+
+class ImageReceiver {
+public:
+  ImageReceiver() = default;
+
+private:
+  ros::NodeHandle n_;
+  cv::Mat current_frame_;
+};
+
+}  // namespace object_detection
 
 void imageReceiveCallback(const sensor_msgs::ImageConstPtr& msg) {
   cv::Mat frame;
@@ -14,6 +30,15 @@ void imageReceiveCallback(const sensor_msgs::ImageConstPtr& msg) {
   } catch (const std::exception& e) {
     ROS_ERROR(e.what());
   }
+
+  cv::String package_dir = ros::package::getPath("object_detection");
+  cv::String model_path =
+                 cv::utils::fs::join(package_dir, "models/yolov4-tiny.weights"),
+             config_path =
+                 cv::utils::fs::join(package_dir, "models/yolov4-tiny.cfg");
+  object_detection::ObjectDetector detector(
+      model_path, object_detection::DNNType::DARKNET,
+      object_detection::DatasetType::COCO, config_path);
 }
 
 int main(int argc, char** argv) {
