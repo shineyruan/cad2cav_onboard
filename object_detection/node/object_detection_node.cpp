77408fafa4b893cf9ac2053d27ed5f1d9740815f
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-10-30 21:36:28 -0400

    add default apriltag config in main

--- object_detection/node/object_detection_node.cpp
+++ object_detection/node/object_detection_node.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char** argv) {
   ros::init(argc, argv, "object_detection_node");
 
-  object_detection::LandmarkProcessor landmark_processor;
+  object_detection::LandmarkProcessor landmark_processor{"Tag36h11", 0.168};
   ros::spin();
 
   return 0;
